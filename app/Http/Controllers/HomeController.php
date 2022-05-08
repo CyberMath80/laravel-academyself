0c2885d5a05ea10af1c6d3c8992613f0b50dacf1
eraste.mathieu@gmail.com <eraste.mathieu@gmail.com>
2022-05-08 16:40:14 +0200

    Migrations courses, roles & levels

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,10 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(Request $request) {
-
-        $redirect = route('home');
-
-        return redirect($redirect);
+    public function index(Request $request)
+    {
+        return view('home.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\{
+    HomeController
+};
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () { return view('home.index'); })->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
 Route::middleware([
